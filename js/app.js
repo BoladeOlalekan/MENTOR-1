09e2e7b1671a405d2b42ca3742b459e61d97b6b5
Olalekan Oluwaseyi Bolade <lekanseyibolade@gmail.com>
2022-07-26 14:52:26 +0100

    commit including js.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('nav');
+const menuBtn = document.querySelector('.menu-Btn');
+
+menuBtn.addEventListener('click' , () => {
+    nav.classList.add('show-menu')
+});
+
